742ad31459970fb53e0eaf5ceadf9cb9a4a18b50
MoBeau <shhan098@naver.com>
2020-07-28 21:24:04 +0900

    Start

---
+++ ProjectSY/ProjectSY/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProjectSY
+//
+//  Created by 한상현 on 2020/07/28.
+//  Copyright © 2020 한상현. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ProjectSY/ProjectSYTests/ProjectSYTests.swift
@@ -0,0 +1,34 @@
+//
+//  ProjectSYTests.swift
+//  ProjectSYTests
+//
+//  Created by 한상현 on 2020/07/28.
+//  Copyright © 2020 한상현. All rights reserved.
+//
+
+import XCTest
+@testable import ProjectSY
+
+class ProjectSYTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
