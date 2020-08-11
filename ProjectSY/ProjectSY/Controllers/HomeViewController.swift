dbb8fd396823b503186a8a5bb96b3de13d70f9e7
MoBeau <shhan098@naver.com>
2020-08-11 16:39:47 +0900

    작업 일시중지 : 디자인 미완료

--- ProjectSY/ProjectSY/Controllers/HomeViewController.swift
+++ ProjectSY/ProjectSY/Controllers/HomeViewController.swift
@@ -17,15 +17,24 @@ class HomeViewController: UIViewController {
     }
     
     @IBOutlet weak var moreButton: UIButton!
+    @IBOutlet weak var editButton: UIButton!
+    @IBOutlet weak var userEditButton: UIButton!
     @IBOutlet weak var settingGetOutButton: UIButton!
     
     @IBAction func settingOpen(_ sender: UIButton) {
+        // buttons show
         settingGetOutButton.isHidden = false
+        editButton.isHidden = false
+        userEditButton.isHidden = false
     }
     
     @IBAction func settingClose(_ sender: UIButton) {
         settingGetOutButton.isHidden = true
+        editButton.isHidden = true
+        userEditButton.isHidden = true
     }
+    
+    
     /*
     // MARK: - Navigation
 
